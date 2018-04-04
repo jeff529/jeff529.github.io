920204f2c49c8c1ab937f20f62c875eda5fc4f0b
Jeff Davout <geoffroy.davout@gmail.com>
2018-04-04 22:28:56 +0100

    Day 2

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+let element = document.getElementById("day01");
+let element2 = document.getElementById("day01Description");
+
+element.addEventListener("click", function() {
+    element2.classList.toggle("openDay");
+});
+
+
